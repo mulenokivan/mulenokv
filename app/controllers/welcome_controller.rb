3fdec6178c1f72f80ecf845bb3cf78743edfc540
mulenokv <mulenokivan@mail.ru>
2023-12-29 13:36:45 +0700

    switch theme

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def home
   end
+
+  def set_theme
+    # if params[:theme].present? && params[:theme].in? ['dark', 'light']
+      cookies[:theme] = params[:theme]
+      redirect_to(request.referrer || root_path)
+    # end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resources :elements
   end
   get "up" => "rails/health#show", as: :rails_health_check
+  get "set_theme", to: "welcome#set_theme"
   root "welcome#home", as: :home
 end
