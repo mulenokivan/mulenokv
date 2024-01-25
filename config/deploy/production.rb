2d08a26ba457b9c3337ac88cff7c103705904f7a
mulenokv <mulenokivan@mail.ru>
2024-01-25 15:05:05 +0700

    again

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 set :rvm_type, :user                # Defaults to: :auto
 set :rvm_ruby_version, '3.3.0'      # Defaults to: 'default'
-server "mulenok.ru", user: "deploy", roles: %w{app db web}, my_property: :my_value
+set :rails_env, "production"
+server "mulenok.ru", user: "deploy", roles: %w{app db web}
