d9df772bc7fbcc26880e744388a88718cb948179
mulenokv <mulenokivan@mail.ru>
2024-01-25 15:49:43 +0700

    again

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,5 +3,8 @@ set :repo_url, "git@github.com:mulenokivan/mulenokv.git"
 set :branch, "main"
 set :deploy_to, "/home/deploy/#{fetch :application}"
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+set :linked_files, %w[config/database.yml config/master.key]
 append :linked_files,  'config/credentials/production.key'
 set :keep_releases, 3
+
+before 'deploy:starting', 'config_files:upload'
